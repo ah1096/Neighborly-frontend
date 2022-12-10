737e24bdde9cfdf0f5722bc701835a8ee654bbc5
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-12-10 13:38:16 +0000

    added SideNav to profile page

--- neighborly/src/components/ProfilePage.js
+++ neighborly/src/components/ProfilePage.js
@@ -5,16 +5,7 @@ import SideNav from './SideNav.js';
 export default function ProfilePage(){
     return(
         <span>
-            <div className="row">
-                <div className="col-3">
-                    <SideNav />
-                </div>
-
-                <div className="col-6">
-                    <GetUser />
-                </div>
-                
-            </div>
+            <GetUser />
         </span>
     )
 
